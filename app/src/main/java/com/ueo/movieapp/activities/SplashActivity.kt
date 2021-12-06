e0e6d722a39f827819e54dda2a91689df61fc1a2
Pal Levente <levi@mobiversal.com>
2021-12-06 11:20:42 +0200

    spalsh start

--- app/src/main/java/com/ueo/movieapp/activities/SplashActivity.kt
+++ app/src/main/java/com/ueo/movieapp/activities/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.ueo.movieapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.ueo.movieapp.MainActivity
 import com.ueo.movieapp.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,6 +12,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val action = Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        findViewById<ImageView>(R.id.iv_icon).postDelayed(action, 2000L)
     }
 
 }
