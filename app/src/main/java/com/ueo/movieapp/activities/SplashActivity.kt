bfb4e627588ad7aefb3ce74e8471b67fd8868d00
Pal Levente <levi@mobiversal.com>
2021-12-06 11:05:43 +0200

    added splash

--- app/src/main/java/com/ueo/movieapp/activities/SplashActivity.kt
+++ app/src/main/java/com/ueo/movieapp/activities/SplashActivity.kt
@@ -2,12 +2,13 @@ package com.ueo.movieapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ueo.movieapp.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.)
+        setContentView(R.layout.activity_splash)
     }
 
 }
